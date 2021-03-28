4de64015cddf3283aac58d625d38404a62ba66aa
crypt0 <acvjetan@gmail.com>
2021-03-28 10:53:00 +0200

    Add connection to database.

--- MERN-Instagram-BE/server.js
+++ MERN-Instagram-BE/server.js
@@ -2,16 +2,19 @@ import express from 'express';
 import cors from 'cors';
 import mongoose from 'mongoose';
 import Pusher from 'pusher';
+import dotenv from 'dotenv';
 
 // App config
 const app = express();
 const PORT = process.env.PORT || 8080;
+dotenv.config();
 
 // Middleware
 app.use(express.json())
 app.use(cors())
 
 // DB config
+const connection_url = process.env.MONGO_URL;
 
 // API routes
 app.get('/', ( (req,res) => {
