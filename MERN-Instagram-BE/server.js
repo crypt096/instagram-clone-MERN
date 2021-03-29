ba94125218ee8423b2177d3e609afce4bebf18db
crypt0 <acvjetan@gmail.com>
2021-03-29 16:34:13 +0200

    Add image to database.

--- MERN-Instagram-BE/server.js
+++ MERN-Instagram-BE/server.js
@@ -45,7 +45,13 @@ app.post('/upload',(req,res) => {
 })
 
 app.get('/sync', (req,res) => {
-    
+    dbModel.find((err, data) => {
+        if(err) {
+            res.status(500).send(err);
+        }else{
+            res.status(200).send(data);
+        }
+    })
 })
 
 // Listeners
